1fbd037520f67c315b91dd43c68cec8ff6c74122
ChunTaoPengim <ChunTaoPengim@github.com>
2023-12-30 00:35:49 +0800

    final

--- hack2/src/app/projects/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProjectsPage() {
-  return (
-    <main className="flex h-full w-full items-center justify-center border">
-      <h1 className="text-xl text-gray-600/50">Please select a project</h1>
-    </main>
-  );
-}
--- hw5/README.md
+++
@@ -1,3 +0,0 @@
-# Web Programming HW#5
-deploy 的網址 https://wp1121-akwc-git-main-chuntaopengs-projects.vercel.app/projects
-hack 2
