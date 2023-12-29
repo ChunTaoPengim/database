8734d8daadd2a4229219601118da56e989e76d76
ChunTaoPengim <ChunTaoPengim@github.com>
2023-12-30 00:32:45 +0800

    preserve final

--- hw2/frontend/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite' // eslint-disable-line
-import react from '@vitejs/plugin-react' // eslint-disable-line
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
